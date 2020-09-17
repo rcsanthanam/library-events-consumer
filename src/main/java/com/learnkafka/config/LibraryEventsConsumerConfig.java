c299ba2a4039e3e2c090608fd580f41f2f1956dc
rcsanthanam <63517325+rcsanthanam@users.noreply.github.com>
2020-09-18 02:42:19 +0530

    Add concurrency feature to Kafka consumer
    This is recommended for non-cloud environment

--- src/main/java/com/learnkafka/config/LibraryEventsConsumerConfig.java
+++ src/main/java/com/learnkafka/config/LibraryEventsConsumerConfig.java
@@ -7,7 +7,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
-import org.springframework.kafka.listener.ContainerProperties.AckMode;
 
 @Configuration
 @EnableKafka
@@ -19,6 +18,7 @@ public class LibraryEventsConsumerConfig {
 	    ObjectProvider<ConsumerFactory<Object, Object>> kafkaConsumerFactory) {
 	ConcurrentKafkaListenerContainerFactory<Object, Object> factory = new ConcurrentKafkaListenerContainerFactory<>();
 	configurer.configure(factory,kafkaConsumerFactory.getIfAvailable());
+	factory.setConcurrency(3);
 	//factory.getContainerProperties().setAckMode(AckMode.MANUAL);
 	return factory;
     }
